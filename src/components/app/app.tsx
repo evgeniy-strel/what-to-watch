20bb2f7686e113a99905a5f7dfa70715a17f6aa7
Evgeniy <evgeniy_strel@yandex.ru>
2022-09-22 01:13:19 +0500

    2.9 homework

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,25 +1,25 @@
-import Main from '../../pages/Main';
+import Main from "../../pages/Main";
 
 const filmsList = [
   {
     id: 0,
-    imgUrl: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
-    title: 'Fantastic Beasts: The Crimes of Grindelwald'
+    imgUrl: "img/fantastic-beasts-the-crimes-of-grindelwald.jpg",
+    title: "Fantastic Beasts: The Crimes of Grindelwald",
   },
   {
     id: 1,
-    imgUrl: 'img/bohemian-rhapsody.jpg',
-    title: 'Bohemian Rhapsody'
+    imgUrl: "img/bohemian-rhapsody.jpg",
+    title: "Bohemian Rhapsody",
   },
   {
     id: 2,
-    imgUrl: 'img/macbeth.jpg',
-    title: 'Macbeth'
+    imgUrl: "img/macbeth.jpg",
+    title: "Macbeth",
   },
   {
     id: 3,
-    imgUrl: 'img/aviator.jpg',
-    title: 'Aviator'
+    imgUrl: "img/aviator.jpg",
+    title: "Aviator",
   },
 ];
 
